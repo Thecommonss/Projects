db5e9c1b46e5ce8cca9f40496401fa88de5e27fe
Thecommonss <133498151+Thecommonss@users.noreply.github.com>
2026-06-03 17:32:33 +0700

    Add files via upload

--- Projectile_Simulator.py
+++ Projectile_Simulator.py
@@ -1,3 +1,4 @@
+import math as m
 import numpy as np
 import matplotlib.pyplot as plt
 
@@ -9,35 +10,88 @@ g = 9.81 # m/s^2
 
 def compute_trajectory(v0,angle):
 
-## Calculate time of flight first with the given v0 and angle
-
-    # From y = Uyt - 1/2 g t^2 ; Set y = 0
-    #      0 = Uyt - 1/2 g t^2
-    #      1/2gt^2 = Uyt
-    #      t = 2v0sin(theta)/g
-    
     theta = np.deg2rad(angle)
-    t_of_flight = 2 * v0 * np.sin(theta) / g
-    t = np.linspace(0,t_of_flight,100)
 
-#### Calulate the X and Y positions
+## Calculate time of flight first with the given v0 and angle
+    opt = input("Do you want to calulate with variable height (y/n): ").lower()
+    if opt == "n":
 
-    x = v0 * np.cos(theta) * t
-    y = v0 * np.sin(theta) * t - 1/2 * g * t ** 2
+        # From y = Uyt - 1/2 g t^2 ; Set y = 0
+        #      0 = Uyt - 1/2 g t^2
+        #      1/2gt^2 = Uyt
+        #      t = 2v0sin(theta)/g
+        
+        
+        t_of_flight = 2 * v0 * np.sin(theta) / g
+        t = np.linspace(0,t_of_flight,100)
 
-    max_y = np.max(y)
-    x_pos_at_max_height = x[np.argmax(y)]
+    #### Calulate the X and Y positions
 
-    return x,y,max_y,x_pos_at_max_height,t
+        x = v0 * np.cos(theta) * t
+        y = v0 * np.sin(theta) * t - 1/2 * g * t ** 2
 
-def statistics(t,x,y):
-    print(f"\nThe time of flight is {np.max(t):.2f} sec.\nRange : {np.max(x):.2f} m.\nHeight : {np.max(y):.2f} m.")
+        max_y = np.max(y)
+        x_pos_at_max_height = x[np.argmax(y)]
+            
+        return x,y,max_y,x_pos_at_max_height,t
+    
+    
+    elif opt == "y":
+       
+        y0 = float(input("Enter the inital height: "))
+        
+        
+        # From Yf - Yi = visin(theta)t - 1/2 gt ** 2
+        # set Yf = 0 ; 0 = visin(theta)t - 1/2 gt ** 2 + yi
+        # 1/2gt**2 - visin(theta) - yi = 0
+        # Solve with quadratic equation
+        # t = -b +- root(b**2-4ac)/2a
+        # where a = 1/2g, b = -visin(theta), c = -Yi
+        # Solve for t
+
+        a = -(1/2 * g)
+        b = v0 * np.sin(theta)
+        c = y0
+
+        d = b**2 - 4*(a*c)
+        
+        if d > 0:
+            
+            t_of_flight = (-b - m.sqrt(d)) / (2 * a)
+
+        elif d == 0:
+
+            t_of_flight = -b / (2 * a)
+
+
+
+        t = np.linspace(0,t_of_flight,100)
+
+        #### Calulate X and Y postions
+
+        x = v0 * np.cos(theta) * t
+        y = v0 * np.sin(theta) * t - 1/2 * g * t ** 2 + y0
+
+        max_y = np.max(y)
+        x_pos_at_max_height = x[np.argmax(y)]
+
+        return x,y,max_y,x_pos_at_max_height,t
+
+
+    else:
+        print("Enter y/n")
+        return
+
+def statistics(t,x,y,angle):
+    print(f"\nThe time of flight is {np.max(t):.2f} sec.\nRange : {np.max(x):.2f} m.\nHeight : {np.max(y):.2f} m.\nAngle : {angle:.2f} Degress")
 
 
 def plot_graph(x,y,max_y,x_pos_at_max_height):
-    
+
+
     plt.plot(x,y, 'b-' , label = 'Projectile')
     plt.plot(x_pos_at_max_height,max_y, 'ro', markersize = 8, label = 'Highest point')
+    plt.plot(x[-1],y[-1], 'go', markersize = 8, label = "Landing point")
     plt.title("Trajectory")
     plt.xlabel("Range (m)")
     plt.ylabel("Height (m)")
@@ -97,13 +151,13 @@ q.) Exit
                     if opt == "n":
                         ##### Print Statistics of the projectile #####
 
-                        statistics(t,x,y)
+                        statistics(t,x,y,angle)
 
                     ##### Plot Graph #####
                     
                     else:
                         
-                        statistics(t,x,y)
+                        statistics(t,x,y,angle)
 
                         # plot graph
 
@@ -146,13 +200,13 @@ q.) Exit
 
                     if opt == "n":
                         
-                        statistics(t1,x1,y1)
-                        statistics(t2,x2,y2)
+                        statistics(t1,x1,y1,angle1)
+                        statistics(t2,x2,y2,angle2)
 
                     else:
 
-                        statistics(t1,x1,y1)
-                        statistics(t2,x2,y2)
+                        statistics(t1,x1,y1,angle1)
+                        statistics(t2,x2,y2,angle2)
 
                         plot_graph(x1,y1,max_y1,x_pos_at_max_height1)
                         plot_graph(x2,y2,max_y2,x_pos_at_max_height2)
